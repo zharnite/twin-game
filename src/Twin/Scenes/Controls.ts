d99c985d5c9124a18e4484cd4c9e3ae9ba0174ee
Zhen Yi Pan <zharnite@gmail.com>
2021-04-08 21:12:07 -0400

    able to update screens with json objects

--- src/Twin/Scenes/Controls.ts
+++ src/Twin/Scenes/Controls.ts
@@ -7,31 +7,66 @@ import Button from "../../Wolfie2D/Nodes/UIElements/Button";
 import MainMenu from "./MainMenu";
 
 export default class Controls extends Scene {
-
-  loadScene(): void {}
+  loadScene(): void {
+    this.load.object("Controls", "assets/texts/controls.json");
+  }
 
   startScene(): void {
     // TODO - Add real text to display controls
 
     // Testing stuff
-    this.addUILayer("Controls");
-    let controlsLabel = <Label>this.add.uiElement(
-      UIElementType.LABEL,
+    let layer = "Controls";
+    this.addUILayer(layer);
+
+    // Zhen TODO - these functions can probably be put in a component and reused across the other Scenes
+    this.createHeadingLabel(layer, layer);
+    this.createTextBody(layer);
+    this.createReturnButton(1000, 730);
+  }
+
+  createHeadingLabel(layer: string, heading: string): void {
+    let halfSize = this.viewport.getHalfSize();
+    let label = <Label>this.add.uiElement(UIElementType.LABEL, layer, {
+      position: new Vec2(halfSize.x, 100),
+      text: heading,
+    });
+    label.fontSize = 50;
+    label.textColor = Color.WHITE;
+    label.font = "Monospace";
+  }
+
+  createTextBody(layer: string): void {
+    let obj = this.load.getObject(layer);
+    let body = obj[layer];
+
+    let halfSize = this.viewport.getHalfSize();
+    let vertex = new Vec2(300, 200);
+    let space = 50;
+    let i = 1;
+    body.forEach((str: string) => {
+      let label = <Label>this.add.uiElement(UIElementType.LABEL, layer, {
+        position: new Vec2(vertex.x, vertex.y + space * i),
+        text: "- " + str,
+      });
+      label.setHAlign("left");
+      label.textColor = Color.WHITE;
+      label.font = "Monospace";
+      i++;
+    });
+  }
+
+  createReturnButton(x: number, y: number): void {
+    // Create return button
+    let returnButton = <Button>this.add.uiElement(
+      UIElementType.BUTTON,
       "Controls",
       {
-        position: new Vec2(100, 100),
-        text: "Controls",
+        position: new Vec2(x, y),
+        text: "Return",
       }
     );
-    controlsLabel.textColor = Color.WHITE;
 
-    // Return to Main Menu button
-    let returnButton = <Button>(
-      this.add.uiElement(UIElementType.BUTTON, "Controls", {
-        position: new Vec2(1000, 730),
-        text: "Return",
-      })
-    );
+    // Apply button styles
     returnButton.applyButtonStyle(
       Color.WHITE,
       Color.BLACK,
@@ -39,7 +74,7 @@ export default class Controls extends Scene {
       "NoPixel"
     );
 
-    // When the control button is clicked, go to the controls screen
+    // Add onClick back to MainMenu
     returnButton.onClick = () => {
       this.sceneManager.changeToScene(MainMenu, {});
     };
