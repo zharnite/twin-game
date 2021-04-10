c69601dae1b8a34820034720c90ef5ace701d3ee
Zhen Yi Pan <zharnite@gmail.com>
2021-04-09 22:49:40 -0400

    created base Splash.ts

--- src/Twin/Scenes/Splash.ts
+++ src/Twin/Scenes/Splash.ts
@@ -1,1 +1,8 @@
 // Twin TODO - make something for this; clicking this screen should bring the user to the MainMenu screen
+import Scene from "../../Wolfie2D/Scene/Scene";
+
+export default class Splash extends Scene {
+  loadScene(): void {}
+  startScene(): void {}
+  updateScene(): void {}
+}
