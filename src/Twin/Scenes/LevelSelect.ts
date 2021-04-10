5c1792f0cd05b1423d60bd353d395c87213e9164
Zhen Yi Pan <zharnite@gmail.com>
2021-04-09 23:17:58 -0400

    created base LevelSelect.ts

--- src/Twin/Scenes/LevelSelect.ts
+++ src/Twin/Scenes/LevelSelect.ts
@@ -1,1 +1,9 @@
 // Twin TODO - Make buttons and color them a color for our level selection; make a link to Level1 for core game mechanics
+
+import Scene from "../../Wolfie2D/Scene/Scene";
+
+export default class LevelSelect extends Scene {
+  loadScene(): void {}
+  startScene(): void {}
+  updateScene(): void {}
+}
