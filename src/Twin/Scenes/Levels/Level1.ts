bf25ee2334be9051dcf559595d3c995dca6814a2
Zhen Yi Pan <zharnite@gmail.com>
2021-04-11 23:55:53 -0400

    added Twin TODOs

--- src/Twin/Scenes/Levels/Level1.ts
+++ src/Twin/Scenes/Levels/Level1.ts
@@ -3,6 +3,7 @@
 // Twin TODO [Benchmark 2] (Code & Art) - Make levels; read "World Rendering" part of  Benchmark 2
 // Twin TODO [Benchmark 2] (Code) - Figure out file format for levels
 // Twin TODO (Code & Art) - Update to the correct player and ghost sprites for all levels
+// Twin TODO (Code) - Pause game
 
 import Vec2 from "../../../Wolfie2D/DataTypes/Vec2";
 import Debug from "../../../Wolfie2D/Debug/Debug";
