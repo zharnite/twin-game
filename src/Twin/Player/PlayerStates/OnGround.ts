19fe858b02ef6cbed22df83a6638e62d28bc82f7
Zhen Yi Pan <zharnite@gmail.com>
2021-04-13 20:10:22 -0400

    updated characterType in OnGround.ts

--- src/Twin/Player/PlayerStates/OnGround.ts
+++ src/Twin/Player/PlayerStates/OnGround.ts
@@ -5,41 +5,40 @@ import MathUtils from "../../../Wolfie2D/Utils/MathUtils";
 import PlayerState from "./PlayerState";
 
 export default class OnGround extends PlayerState {
-	onEnter(options: Record<string, any>): void {}
+  onEnter(options: Record<string, any>): void {}
 
-	handleInput(event: GameEvent): void {}
+  handleInput(event: GameEvent): void {}
 
-	update(deltaT: number): void {
-		if(this.parent.velocity.y > 0){
-			this.parent.velocity.y = 0;
-		}
-		super.update(deltaT);
+  update(deltaT: number): void {
+    if (this.parent.velocity.y > 0) {
+      this.parent.velocity.y = 0;
+    }
+    super.update(deltaT);
 
-		let direction = this.getInputDirection();
+    let direction = this.getInputDirection();
 
-		if(direction.x !== 0){
-			(<Sprite>this.owner).invertX = MathUtils.sign(direction.x) < 0;
-		}
+    if (direction.x !== 0) {
+      (<Sprite>this.owner).invertX = MathUtils.sign(direction.x) < 0;
+    }
 
-		if(Input.isJustPressed("jump")){
-			this.finished("jump");
-			// Change jump height based on if the jumping character is the soul or body.
-			if (this.parent.characterType === 1) {
-				this.parent.velocity.y = -350;
-			}
-			else {
-				this.parent.velocity.y = -550;
-			}
-			if(this.parent.velocity.x !== 0){
-				// We don't need this silly flip right now.
-				// this.owner.tweens.play("flip");
-			}
-		} else if(!this.owner.onGround){
-			this.finished("jump");
-		}
-	}
+    if (Input.isJustPressed("jump")) {
+      this.finished("jump");
+      // Change jump height based on if the jumping character is the soul or body.
+      if (this.parent.characterType === "body") {
+        this.parent.velocity.y = -350;
+      } else {
+        this.parent.velocity.y = -550;
+      }
+      if (this.parent.velocity.x !== 0) {
+        // We don't need this silly flip right now.
+        // this.owner.tweens.play("flip");
+      }
+    } else if (!this.owner.onGround) {
+      this.finished("jump");
+    }
+  }
 
-	onExit(): Record<string, any> {
-		return {};
-	}
+  onExit(): Record<string, any> {
+    return {};
+  }
 }
