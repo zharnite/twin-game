9936d540becc728dde872d250c69ac2097c4aae2
Zhen Yi Pan <zharnite@gmail.com>
2021-04-16 13:12:05 -0400

    cleaned Credits.ts and Help.ts

--- src/Twin/Scenes/Screens/InfoScreens/Credits.ts
+++ src/Twin/Scenes/Screens/InfoScreens/Credits.ts
@@ -1,34 +1,32 @@
 import Scene from "../../../../Wolfie2D/Scene/Scene";
-import MainMenu from "../MainMenu";
-import SceneItemCreator from "../../SceneHelpers/SceneItemCreator";
+import { Screens } from "../../Enums/ScreenEnums";
+import { ScreenTexts } from "../../Enums/ScreenTextEnums";
+import InfoScreenCreator from "../../SceneHelpers/InfoScreenCreator";
 
 export default class Credits extends Scene {
-  private init: Record<string, any>;
+  private layer: string;
 
   loadScene(): void {
     this.load.object("Credits", "assets/texts/credits.json");
   }
 
-  initScene(init: Record<string, any>): void {
-    this.init = init;
+  startScene(): void {
+    // Create Credits layer
+    this.layer = Screens.CREDITS;
+    this.addUILayer(this.layer);
+
+    // Create screen objects
+    this.createScreen();
   }
 
-  startScene(): void {
-    let layer = "Credits";
-    this.addUILayer(layer);
-
-    SceneItemCreator.createHeadingLabel(this, this.viewport, layer, "CREDITS");
-    SceneItemCreator.createTextBody(this, this.viewport, layer, 600);
-    // return button
-    SceneItemCreator.createButton(
+  private createScreen(): void {
+    let isc = new InfoScreenCreator(
       this,
-      layer,
-      1000,
-      730,
-      "RETURN"
-    ).onClick = () => {
-      this.sceneManager.changeToScene(MainMenu, {});
-    };
+      this.viewport,
+      this.layer,
+      this.sceneManager
+    );
+    isc.createScreen(ScreenTexts.CREDITS);
   }
 
   updateScene(): void {}
--- src/Twin/Scenes/Screens/InfoScreens/Help.ts
+++ src/Twin/Scenes/Screens/InfoScreens/Help.ts
@@ -1,28 +1,34 @@
 import Scene from "../../../../Wolfie2D/Scene/Scene";
 import MainMenu from "../MainMenu";
 import SceneItemCreator from "../../SceneHelpers/SceneItemCreator";
+import { Screens } from "../../Enums/ScreenEnums";
+import InfoScreenCreator from "../../SceneHelpers/InfoScreenCreator";
+import { ScreenTexts } from "../../Enums/ScreenTextEnums";
 
 export default class Help extends Scene {
+  private layer: string;
+
   loadScene(): void {
     this.load.object("Help", "assets/texts/help.json");
   }
 
   startScene(): void {
-    let layer = "Help";
-    this.addUILayer(layer);
+    // Create Help layer
+    this.layer = Screens.HELP;
+    this.addUILayer(this.layer);
 
-    SceneItemCreator.createHeadingLabel(this, this.viewport, layer, "HELP");
-    SceneItemCreator.createTextBody(this, this.viewport, layer, 600);
-    // return button
-    SceneItemCreator.createButton(
+    // Create screen objects
+    this.createScreen();
+  }
+
+  private createScreen(): void {
+    let isc = new InfoScreenCreator(
       this,
-      layer,
-      1000,
-      730,
-      "RETURN"
-    ).onClick = () => {
-      this.sceneManager.changeToScene(MainMenu, {});
-    };
+      this.viewport,
+      this.layer,
+      this.sceneManager
+    );
+    isc.createScreen(ScreenTexts.HELP);
   }
 
   updateScene(): void {}
