0f450239aa69e269afb60f917bbeafe91886ff94
Zhen Yi Pan <zharnite@gmail.com>
2021-04-16 12:21:47 -0400

    added ScreenEnums.ts

---
+++ src/Twin/Scenes/Enums/ScreenEnums.ts
@@ -0,0 +1,8 @@
+export enum Screen {
+  CONTROLS = "ControlsScreen",
+  CREDITS = "CreditsScreen",
+  HELP = "HelpScreen",
+  LEVEL_SELECT = "LevelSelectScreen",
+  MAIN_MENU = "MainMenuScreen",
+  SPLASH = "SplashScreen",
+}
